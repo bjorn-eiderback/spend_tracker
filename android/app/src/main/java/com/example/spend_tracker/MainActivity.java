399eeea570e139bcefa8e91d05afbfa59ed47ce5
Björn Eiderbäck <bjorn.eiderback@gmail.com>
2021-05-16 14:59:53 +0200

    Lite ändringar

--- android/app/src/main/java/com/example/spend_tracker/MainActivity.java
+++ android/app/src/main/java/com/example/spend_tracker/MainActivity.java
@@ -1,13 +1,3 @@
 package com.example.spend_tracker;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
-public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
-}
+import import io.flutter.embedding.android.FlutterActivity;
